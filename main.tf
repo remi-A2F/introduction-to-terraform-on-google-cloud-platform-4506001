fa1c4d75298a17a720c2fc12bae6d7123cf2e9e1
remi-A2F <73338502+remi-A2F@users.noreply.github.com>
2025-10-15 14:53:38 +0200

    qa staging prod

--- main.tf
+++ main.tf
@@ -4,6 +4,20 @@ module "qa" {
     network_name = "qa"
     project_id = var.project_id
     region = var.region
-    
+}
 
+module "staging" {
+    source = "./modules/blog"
+    app_name= "staging-blog"
+    network_name = "staging"
+    project_id = var.project_id
+    region = var.region
+}
+
+module "prod" {
+    source = "./modules/blog"
+    app_name= "prod-blog"
+    network_name = "prod"
+    project_id = var.project_id
+    region = var.region
 }
