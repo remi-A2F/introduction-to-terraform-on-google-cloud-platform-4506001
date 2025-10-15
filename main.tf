4ca0226a7cc70d905f04dfec76c9be7ebe44eb85
remi-A2F <73338502+remi-A2F@users.noreply.github.com>
2025-10-15 11:44:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ data "google_compute_image" "ubuntu" {
 resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
-
+tags = ["${var.network_name}-web"]
   
   boot_disk {
     initialize_params {
@@ -49,6 +49,6 @@ resource "google_compute_instance" "blog" {
       # Leave empty for dynamic public IP
     }
   }  
-
+metadata_start_script = "apt -y update install nginx; echo ${var.app_name} > /var/www/html/index.html"
   allow_stopping_for_update = true
 }
